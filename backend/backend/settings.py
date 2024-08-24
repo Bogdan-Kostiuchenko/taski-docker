b5f186f470f7ce1bf9ccad6cb131a635194da676
Bogdan Kostiuchenko <bbkostyuchenko@gmail.com>
2024-08-24 16:26:33 +0200

    Add flake8 instructions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,4 @@
 import os
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,8 +70,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
